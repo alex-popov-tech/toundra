0e9cad5d6b3184b726b7843b8e42d1a1f9fbc721
Alexander Popov <aleksander.popov.93@gmail.com>
2018-12-16 22:07:52 +0200

    fix relative paths

--- built/configuration.d.ts
+++ built/configuration.d.ts
@@ -1,6 +1,6 @@
 export declare namespace Configuration {
-    const BIN_PATH: string;
-    const MASTER_EXECUTOR_PATH: string;
-    const WORKER_EXECUTOR_PATH: string;
+    const BIN_PATH = "./built/bin/samael.js";
+    const MASTER_EXECUTOR_PATH = "../master/execute.js";
+    const WORKER_EXECUTOR_PATH = "../worker/execute.js";
     const GLOBAL_SUITE_NAME = "GLOBAL";
 }
--- lib/configuration.ts
+++ lib/configuration.ts
@@ -1,8 +1,11 @@
 import * as path from 'path';
 
 export namespace Configuration {
-    export const BIN_PATH = path.resolve('./built/bin/samael.js');
-    export const MASTER_EXECUTOR_PATH = path.resolve('./built/master/execute.js');
-    export const WORKER_EXECUTOR_PATH = path.resolve('./built/worker/execute.js');
+    // export const BIN_PATH = path.resolve('./built/bin/samael.js');
+    // export const MASTER_EXECUTOR_PATH = path.resolve('./built/master/execute.js');
+    // export const WORKER_EXECUTOR_PATH = path.resolve('./built/worker/execute.js');
+    export const BIN_PATH = './built/bin/samael.js';
+    export const MASTER_EXECUTOR_PATH = '../master/execute.js';
+    export const WORKER_EXECUTOR_PATH = '../worker/execute.js';
     export const GLOBAL_SUITE_NAME = 'GLOBAL';
 }
