28b099f8ad43c3c1a91e7bcda329cfa47df69cf1
Alexander Popov <aleksander.popov.93@gmail.com>
2018-12-16 22:26:59 +0200

    --amend

--- built/configuration.d.ts
+++ built/configuration.d.ts
@@ -1,5 +1,5 @@
 export declare namespace Configuration {
-    const BIN_PATH = "./built/bin/samael.js";
+    const BIN_PATH = "./node_modules/samael/built/bin/samael.js";
     const MASTER_EXECUTOR_PATH = "../master/execute.js";
     const WORKER_EXECUTOR_PATH = "../worker/execute.js";
     const GLOBAL_SUITE_NAME = "GLOBAL";
--- built/configuration.js
+++ built/configuration.js
@@ -5,7 +5,7 @@ var Configuration;
     // export const BIN_PATH = path.resolve('./built/bin/samael.js');
     // export const MASTER_EXECUTOR_PATH = path.resolve('./built/master/execute.js');
     // export const WORKER_EXECUTOR_PATH = path.resolve('./built/worker/execute.js');
-    Configuration.BIN_PATH = './built/bin/samael.js';
+    Configuration.BIN_PATH = './node_modules/samael/built/bin/samael.js';
     Configuration.MASTER_EXECUTOR_PATH = '../master/execute.js';
     Configuration.WORKER_EXECUTOR_PATH = '../worker/execute.js';
     Configuration.GLOBAL_SUITE_NAME = 'GLOBAL';
