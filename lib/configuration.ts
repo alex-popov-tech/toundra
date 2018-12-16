39bd216ebb3d044c7388f941807ed57ae8b08ca1
Alexander Popov <aleksander.popov.93@gmail.com>
2018-12-16 22:25:20 +0200

    add strange path to bin

--- lib/configuration.ts
+++ lib/configuration.ts
@@ -4,7 +4,7 @@ export namespace Configuration {
     // export const BIN_PATH = path.resolve('./built/bin/samael.js');
     // export const MASTER_EXECUTOR_PATH = path.resolve('./built/master/execute.js');
     // export const WORKER_EXECUTOR_PATH = path.resolve('./built/worker/execute.js');
-    export const BIN_PATH = './built/bin/samael.js';
+    export const BIN_PATH = './node_modules/samael/built/bin/samael.js';
     export const MASTER_EXECUTOR_PATH = '../master/execute.js';
     export const WORKER_EXECUTOR_PATH = '../worker/execute.js';
     export const GLOBAL_SUITE_NAME = 'GLOBAL';
