b2f24bc224b95ec4c98396fc9f4742a02bd0d5e0
Alexander Popov <aleksander.popov.93@gmail.com>
2018-12-17 22:51:17 +0200

    add magic paths

--- built/configuration.js
+++ built/configuration.js
@@ -4,12 +4,17 @@ const consoleListener_1 = require("./listener/consoleListener");
 var Configuration;
 (function (Configuration) {
     const path = require('path');
-    Configuration.BIN_PATH = path.resolve('./built/bin/samael.js');
-    Configuration.MASTER_EXECUTOR_PATH = path.resolve('./built/master/execute.js');
-    Configuration.WORKER_EXECUTOR_PATH = path.resolve('./built/worker/execute.js');
-    // export const BIN_PATH = './node_modules/samael/built/bin/samael.js';
-    // export const MASTER_EXECUTOR_PATH = '../master/execute.js';
-    // export const WORKER_EXECUTOR_PATH = '../worker/execute.js';
+    const fs = require('fs');
+    // paths magic
+    Configuration.BIN_PATH = fs.existsSync(path.resolve('./built/bin/samael.js'))
+        ? path.resolve('./built/bin/samael.js')
+        : './node_modules/samael/built/bin/samael.js';
+    Configuration.MASTER_EXECUTOR_PATH = fs.existsSync(path.resolve('./built/master/execute.js'))
+        ? path.resolve('./built/master/execute.js')
+        : '../master/execute.js';
+    Configuration.WORKER_EXECUTOR_PATH = fs.existsSync(path.resolve('./built/worker/execute.js'))
+        ? path.resolve('./built/worker/execute.js')
+        : '../worker/execute.js';
     Configuration.GLOBAL_SUITE_NAME = 'GLOBAL';
     Configuration.DEFAULT_LISTENER = consoleListener_1.CONSOLE_LISTENER;
 })(Configuration = exports.Configuration || (exports.Configuration = {}));
--- lib/configuration.ts
+++ lib/configuration.ts
@@ -3,12 +3,21 @@ import { CONSOLE_LISTENER } from './listener/consoleListener';
 
 export namespace Configuration {
     const path = require('path');
-    export const BIN_PATH = path.resolve('./built/bin/samael.js');
-    export const MASTER_EXECUTOR_PATH = path.resolve('./built/master/execute.js');
-    export const WORKER_EXECUTOR_PATH = path.resolve('./built/worker/execute.js');
-    // export const BIN_PATH = './node_modules/samael/built/bin/samael.js';
-    // export const MASTER_EXECUTOR_PATH = '../master/execute.js';
-    // export const WORKER_EXECUTOR_PATH = '../worker/execute.js';
+    const fs = require('fs');
+
+    // paths magic
+    export const BIN_PATH = fs.existsSync(path.resolve('./built/bin/samael.js'))
+        ? path.resolve('./built/bin/samael.js')
+        : './node_modules/samael/built/bin/samael.js';
+
+    export const MASTER_EXECUTOR_PATH = fs.existsSync(path.resolve('./built/master/execute.js'))
+        ? path.resolve('./built/master/execute.js')
+        : '../master/execute.js';
+
+    export const WORKER_EXECUTOR_PATH = fs.existsSync(path.resolve('./built/worker/execute.js'))
+        ? path.resolve('./built/worker/execute.js')
+        : '../worker/execute.js';
+
     export const GLOBAL_SUITE_NAME = 'GLOBAL';
     export const DEFAULT_LISTENER = CONSOLE_LISTENER;
 }
