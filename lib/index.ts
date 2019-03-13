e0a3be9934b8848c4dafe92e2ec20fdf2f7169d4
Alexander Popov <alex.popov.tech@gmail.com>
2019-03-13 22:47:51 +0200

    add camel case exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,33 @@
 import { Api } from './api';
 
 const Test = Api.Test;
+const test = Api.Test;
 const Suite = Api.Suite;
+const suite = Api.Suite;
 const BeforeEach = Api.BeforeEach;
+const beforeEach = Api.BeforeEach;
 const BeforeAll = Api.BeforeAll;
+const beforeAll = Api.BeforeAll;
 const AfterEach = Api.AfterEach;
+const afterEach = Api.AfterEach;
 const AfterAll = Api.AfterAll;
+const afterAll = Api.AfterAll;
 const AddListener = Api.AddListener;
+const addListener = Api.AddListener;
 
-export { Test, Suite, BeforeEach, BeforeAll, AfterEach, AfterAll, AddListener };
+export {
+   Test,
+   test,
+   Suite,
+   suite,
+   BeforeEach,
+   beforeEach,
+   BeforeAll,
+   beforeAll,
+   AfterEach,
+   afterEach,
+   AfterAll,
+   afterAll,
+   AddListener,
+   addListener
+};
