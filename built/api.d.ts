4cda8db60cda3bdeb482c5b4743c1f60464462fd
Alexander Popov <alex.popov.tech@gmail.com>
2019-08-05 18:06:39 +0300

    update api

--- built/api.d.ts
+++ built/api.d.ts
@@ -4,9 +4,9 @@ import { Listener } from './listener/listener';
 export declare namespace Api {
     function Suite(name: string, action: SyncAction): void;
     function Test(name: string, action: Action): void;
-    function BeforeAll(hook: () => void | Promise<void>): void;
-    function AfterAll(hook: () => void | Promise<void>): void;
-    function BeforeEach(hook: () => void | Promise<void>): void;
-    function AfterEach(hook: () => void | Promise<void>): void;
+    function BeforeAll(hook: Action): void;
+    function AfterAll(hook: Action): void;
+    function BeforeEach(hook: Action): void;
+    function AfterEach(hook: Action): void;
     function AddListener(listener: Listener): void;
 }
