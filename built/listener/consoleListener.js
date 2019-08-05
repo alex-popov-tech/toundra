f4251eaafc70e719085de3a0d5bdc491650b23d3
Alexander Popov <alex.popov.tech@gmail.com>
2019-08-05 18:49:01 +0300

    update console listener

--- built/listener/consoleListener.js
+++ built/listener/consoleListener.js
@@ -1,21 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const start = new Date().getTime();
+let testsCount = 0;
+const failedTests = [];
 exports.CONSOLE_LISTENER = {
     onStart: (result) => {
         process.stdout.write(`\nTests Started in ${result.threads} thread(s)\n`);
     },
     onTestFinish: result => {
         process.stdout.write(result.error ? 'F' : '.');
+        testsCount++;
+        if (result.error) {
+            failedTests.push({ name: result.name, error: result.error.name, stack: result.error.stack });
+        }
     },
-    onFinish: result => {
-        const testsCount = result.globalTests.length + result.suites.map(suite => suite.tests.length).reduce((l1, l2) => l1 + l2, 0);
-        const passedTests = [...result.globalTests.filter(test => !test.error), ...result.suites.map(suite => suite.tests.filter(test => !test.error))];
-        const failedTests = [...result.globalTests.filter(test => test.error), ...result.suites.map(suite => suite.tests.filter(test => test.error)).reduce((a1, a2) => a1.concat(a2), [])];
-        const errorMessage = 'Errors:\n' + failedTests.map((test, index) => `\t${index + 1}) ${test.name}\n\t\tError message: ${test.error.name}\n\t\tError stacktrace: ${test.error.stack.replace('\n', '\n')}`).join('\n\n');
+    onFinish: _ => {
         process.stdout.write(`\nTests Finished in ${new Date().getTime() - start}ms`);
-        process.stdout.write(`\nOverall tests - ${testsCount}. Passed - ${passedTests.length}. Failed - ${failedTests.length}\n`);
+        process.stdout.write(`\nOverall tests - ${testsCount}. Passed - ${testsCount - failedTests.length}. Failed - ${failedTests.length}\n`);
         if (failedTests.length > 0) {
+            const errorMessage = 'Errors:\n' +
+                failedTests.map((info, i) => `\t${i + 1}) ` + `${info.name}\n` +
+                    `\t\t${info.message}` +
+                    `\t\t\t${info.stack}`).join('\n');
             process.stdout.write(`\n${errorMessage}\n`);
         }
     }
